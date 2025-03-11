c3cf34f2571bce17483598722375ea77077f6c86
Nidhin.B <103826656+nidhinnidhin@users.noreply.github.com>
2025-03-11 16:55:14 +0530

    deployement error fixing 2

--- index.js
+++ index.js
@@ -201,13 +201,7 @@ app.use(passport.session());
 // 1. Configure CORS
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: [
