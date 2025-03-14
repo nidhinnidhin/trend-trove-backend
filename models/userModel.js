8d6304d9552fc9cb175bed90996197b9f8ef63f1
Nidhin.B <103826656+nidhinnidhin@users.noreply.github.com>
2025-03-14 15:05:45 +0530

    deploy succes

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   googleId: { type: String },
   firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
+  lastname: { type: String, required: false },
   username: { type: String, required: true, unique: true, trim: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: false, default: null },
