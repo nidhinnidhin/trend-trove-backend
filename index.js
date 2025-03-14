2138292dad938dadb9fab510c4310629caa7f176
Nidhin.B <103826656+nidhinnidhin@users.noreply.github.com>
2025-03-14 11:50:39 +0530

    done few error fixingg

--- index.js
+++ index.js
@@ -177,30 +177,34 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
+// app.use(cors({
+
+//   origin: function(origin, callback) {
+//     if (!origin) return callback(null, true);
     
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: [
-    'Content-Type', 
-    'Authorization', 
-    'x-csrf-token',
-    'X-Requested-With',
-    'Accept',
-    'Origin'
-  ],
-  exposedHeaders: ['x-csrf-token']
-}));
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+//   allowedHeaders: [
+//     'Content-Type', 
+//     'Authorization', 
+//     'x-csrf-token',
+//     'X-Requested-With',
+//     'Accept',
+//     'Origin'
+//   ],
+//   exposedHeaders: ['x-csrf-token']
+// }));
 
 // CSRF protection setup
+
+ app.use(cors())
+ 
 const csrfProtection = csrf({
   cookie: {
     key: '_csrf',
